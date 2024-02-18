9b5d1fbcfa7f42e397e6bcccbee66d60074ac308
‘ELMILYASS’ <ilyasselmazidi123@gmail.com>
2024-02-18 17:52:27 +0100

    adding command pattern

---
+++ behavioral/command/src/main/java/Light.java
@@ -0,0 +1,9 @@
+public class Light {
+    void turnOn() {
+        System.out.println("Light is on");
+    }
+
+    void turnOff() {
+        System.out.println("Light is off");
+    }
+}
---
+++ behavioral/command/src/main/java/TurnOffCommand.java
@@ -0,0 +1,12 @@
+class TurnOffCommand implements Command {
+    private Light light;
+
+    TurnOffCommand(Light light) {
+        this.light = light;
+    }
+
+    @Override
+    public void execute() {
+        light.turnOff();
+    }
+}
